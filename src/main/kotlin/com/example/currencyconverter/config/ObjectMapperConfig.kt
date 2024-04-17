d2eba08743c6af2fc9f5e8b728197ba290ba5ad1
jeanniehjan@gmail.com <jeanniehjan@gmail.com>
2024-04-17 22:21:51 +0100

    Split configuration into WebClientConfig and ObjectMapperConfig classes

--- src/main/kotlin/com/example/currencyconverter/config/ObjectMapperConfig.kt
+++ src/main/kotlin/com/example/currencyconverter/config/ObjectMapperConfig.kt
@@ -5,16 +5,9 @@ import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
 import com.fasterxml.jackson.module.kotlin.registerKotlinModule
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.web.reactive.function.client.WebClient
 
 @Configuration
-class Config {
-
-    @Bean
-    fun webClient(): WebClient {
-        return WebClient.create()
-    }
-
+class ObjectMapperConfig {
     @Bean
     fun objectMapper(): ObjectMapper {
         return ObjectMapper()
